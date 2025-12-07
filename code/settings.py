c2c7e12d6fd18d3a6302e85ebef3fdc692c84c85
Xinyi Liu <xinyiliu@dhcp-10-250-170-84.harvard.edu>
2025-12-06 19:51:03 -0500

    Newest project updates2

--- code/settings.py
+++ code/settings.py
@@ -2,6 +2,6 @@ import pygame
 from os.path import join
 from os import walk
 
-
+# Abstract away from imports needed for every python script in the code folder. Also indicate the window size of the game and tile size.
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280,720
 TILE_SIZE = 64
--- code/utils.py
+++ code/utils.py
@@ -1,10 +1,7 @@
 import os
 
+# Get absolute path to asset files relative to the project root. Usage: get_asset_path('images', 'player', 'down', '0.png')
 def get_asset_path(*path_parts):
-    """
-    Get absolute path to asset files relative to the project root.
-    Usage: get_asset_path('images', 'player', 'down', '0.png')
-    """
     base_dir = os.path.dirname(os.path.abspath(__file__))
     full_path = os.path.join(base_dir, '..', *path_parts)
     return os.path.normpath(full_path)
